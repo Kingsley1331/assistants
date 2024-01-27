785ef1f01a370ea1a6794057f518446bee6ef7e3
Kingsley Ankomah <kingsley.ankomah@gmail.com>
2024-01-27 15:08:35 +0000

    Update styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: { "twitter-blue": "#1DA1F2" },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
