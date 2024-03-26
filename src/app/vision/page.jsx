69c9c1ceea54d26d92c67e73978d2a85de1be244
Kingsley Ankomah <kingsley.ankomah@gmail.com>
2024-03-26 23:05:32 +0000

    clean up

--- src/app/vision/page.jsx
+++ src/app/vision/page.jsx
@@ -1,9 +1,7 @@
 "use client";
-// import { useChat } from "ai/react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-// import S3UploadForm from "../components/S3UploadForm";
 
 const Chat = () => {
   const [imageUrl, setImageUrl] = useState(null);
@@ -14,20 +12,7 @@ const Chat = () => {
     { role: "system", content: "You are a helpful assistant." },
   ]);
 
-  useEffect(() => {
-    // axios.get("/api/vision").then(({ data }) => {
-    //   console.log("get data ==>", data);
-    //   if (data?.messages) {
-    //     return setMessages(data.messages);
-    //   }
-    // });
-  }, []);
-
   const sendmessage = async (image_url) => {
-    // const response = await axios.post("/api/vision", {
-    //   messages: [...messages, { role: "user", content: userInput }],
-    // });
-
     const content = [{ type: "text", text: userInput }];
 
     if (image_url) {
@@ -45,20 +30,6 @@ const Chat = () => {
           {
             role: "user",
             content,
-            // content: [
-            //   { type: "text", text: userInput },
-            //   ...(selectedFile?.name && {
-            //     type: "image",
-            //     url: imgUrl,
-            //   }),
-            // ],
-            // content: [
-            //   { type: "text", text: userInput },
-            //   {
-            //     type: "image_url",
-            //     image_url,
-            //   },
-            // ],
           },
         ],
       }),
@@ -136,137 +107,24 @@ const Chat = () => {
     }
   };
 
-  // const handleSubmit = () => {
-  //   axios
-  //     .post("/api/vision", {
-  //       messages,
-  //       data: {
-  //         imageUrl,
-  //       },
-  //     })
-  //     .then(({ data }) => {
-  //       console.log("data", data);
-  //       setMessages((msgs) => [...msgs, ...data.messages]);
-  //     });
-  // };
-  // const {
-  //   input,
-  //   handleInputChange,
-  //   handleSubmit,
-  //   isLoading,
-  //   messages,
-  //   setMessages,
-  // } = useChat({
-  //   api: "/api/vision",
-  //   initialInput: "Can you describe this image?",
-  // });
-
-  // setMessages((msgs) => [
-  //   // ...(msgs ? [...msgs] : []),
-  //   {
-  //     role: "user",
-  //     content: [
-  //       { type: "text", text: input },
-  //       ,
-  //       { type: "image_url", image_url: imageUrl },
-  //     ],
-  //   },
-  // ]);
-  // setMessages(messages.map((msg) => ({ ...msg, content: msg.content[0].text })  );
-  // setMessages((msgs) => msgs.concat({ role: "user", content: input });
-
-  // console.log("messages", messages);
-  console.log("selectedFile", selectedFile);
-
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
-  // const handleFileInput = (e) => {
-  //   const file = e.target.files[0];
-
-  //   setSelectedFile(file);
-  //   if (!file) {
-  //     setImageUrl(null);
-  //     setSelectedFile(null);
-  //     return;
-  //   }
-  //   const reader = new FileReader();
-
-  //   reader.onloadend = () => {
-  //     setImageUrl(reader.result);
-  //   };
-
-  //   reader.readAsDataURL(file);
-  // };
-  // console.log("userInput", userInput);
-  console.log("imageUrl", imageUrl);
-
   return (
     <div>
       <h1>Vision</h1>
-      <form
-        className="mt-12"
-        onSubmit={handleFileSubmit}
-        // onSubmit={(e) => {
-        //   handleSubmit(e, {
-        //     data: {
-        //       imageUrl,
-        //       // imageUrl:
-        //       //   "https://vision-model-images1.s3.eu-north-1.amazonaws.com/images/life.jpg",
-        //     },
-        //   });
-        // }}
-        // onSubmit={handleSubmit}
-      >
+      <form className="mt-12" onSubmit={handleFileSubmit}>
         <input
           style={{ width: "100%" }}
           onChange={handleInputChange}
           type="text"
           value={userInput}
           placeholder="Please ask anything you want"
-          // value={input}
         />
         <br />
         <button type="submit">Send</button>
-        {/* <div className="file-input">
-          <input
-            type="file"
-            id="file"
-            className="file"
-            onChange={handleFileInput}
-          />
-          <label htmlFor="file"></label> */}
-        {/* <Image width="100" height="100" src="/getty.jpg" alt="close"></Image> */}
-        {/* <Image
-            width="100"
-            height="100"
-            src="/images/test.jpg"
-            alt="close"
-          ></Image> */}
-        {/* {imageUrl && (
-            <div className="preview">
-              <Image
-                width="100"
-                height="100"
-                src={imageUrl}
-                alt="preview"
-              ></Image>
-              <div
-                className="close"
-                role="button"
-                onClick={() => {
-                  setImageUrl(null);
-                  setSelectedFile(null);
-                }}
-              >
-                {/* <Image width="20" src={close} alt="close"></Image> */}
-        {/* </div>
-            </div>
-          )}
-        </div>  */}
 
-        {/* <button onClick={handleSubmit}>Send</button> */}
         {messages.map((message) => {
           const { content } = message;
           const text = typeof content === "string" ? content : content[0]?.text;
