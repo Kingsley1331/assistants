0ee868ec4110af8003991172eb5a064af8ee4112
Kingsley Ankomah <kingsley.ankomah@gmail.com>
2024-04-14 16:39:48 +0100

    Make request to assistant more efficient

--- src/pages/api/message/[threadId].js
+++ src/pages/api/message/[threadId].js
@@ -2,47 +2,14 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-/**Used in multiple places turn into utility */
-const getAssistants = async () => {
-  const assistantList = (await openai.beta.assistants.list()).data;
-  //   console.log("assistantList ==>", assistantList);
-  return assistantList;
-};
-
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { threadId } = req.query;
     const payload = req.body;
-
     const { userInput } = payload;
-    const assistantList = await getAssistants();
 
-    let assistant = {};
     let streamedResponse = "";
     const { runId, message: userMessage, assistantId } = userInput || {};
-
-    try {
-      if (!assistantList.length) {
-        assistant = await openai.beta.assistants.create({
-          name: "Math Tutor2",
-          instructions:
-            "You are a personal math tutor. Write and run code to answer math questions.",
-          tools: [{ type: "code_interpreter" }],
-          model: "gpt-4-1106-preview",
-          //   file_ids: [file.id]
-        });
-      } else {
-        if (assistantId) {
-          assistant = assistantList.filter(
-            (assist) => assist.id === assistantId
-          )[0];
-
-          console.log("assistant ==>", assistant);
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
 
     // Pass in the user question into the existing thread
     if (userInput) {
@@ -58,10 +25,10 @@ export default async function handler(req, res) {
       });
     }
 
-    if (assistant.id) {
+    if (assistantId) {
       const run = openai.beta.threads.runs
         .stream(threadId, {
-          assistant_id: assistant.id,
+          assistant_id: assistantId,
         })
         .on("textCreated", (text) => {
           console.log(`Response ====> ${text.value}`);
